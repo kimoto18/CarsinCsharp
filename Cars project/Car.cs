d7641b2accc4d9277f3b92edb52edc05f57ed05a
Kyle Kimoto <122324372+kimoto18@users.noreply.github.com>
2023-01-23 20:31:23 -0500

    Car
    first version

---
+++ Cars project/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsProject
+{
+    public class Car
+    {
+        public int year;
+        public string? make;
+        public string? model;
+        public int numberOfDoors;
+        public double price;
+
+    }
+
+ 
+}
---
+++ Cars project/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CarsProject
+{
+    internal class Program
+    {
+        List<Car> cars = new List<Car>();
+        static void Main(string[] args)
+        {
+            // main menu
+            // prompt for user input, show menu until 4
+            string input = "";
+
+            while (input != "4")
+            {
+                Console.WriteLine(" Welcome to Kyle's Car Dealership, where your credit goes to die!");
+                Console.WriteLine(" 1. Add a new car");
+                Console.WriteLine(" 2. Sell a current car");
+                Console.WriteLine(" 3. List all cars");
+                Console.WriteLine(" 4. Quit");
+                input= Console.ReadLine();
+            }
+        }
+         public void AddCar()
+        {
+          Console.WriteLine("Enter the year of the vehicle:");
+          Car car = new Car();
+          int year = int.Parse(Console.ReadLine());
+          car.year = year;
+          Console.WriteLine("Enter the make of the vehicle:");
+          string make = Console.ReadLine();
+          car.make = make;
+          Console.WriteLine("Enter the model of the vehicle:");
+          string model = Console.ReadLine();
+          car.model = model;
+          Console.WriteLine("Enter the number of doors on the vehicle:");
+          int numOfDoors = int.Parse(Console.ReadLine());
+          car.numberOfDoors = numOfDoors;
+          Console.WriteLine("Enter the price of the vehicle:");
+          double price = double.Parse(Console.ReadLine());
+          car.price = price;
+          cars.Add(car);
+          Console.WriteLine("Your car is a:" + car.year.ToString() + " " + car.make + " " + car.model);
+
+        }
+        public void DeleteCar()
+        {
+
+        }
+        public void ListCars()
+        {
+            foreach (Car car in cars) 
+            {
+                Console.WriteLine(car.year.ToString() + " " + car.make + " " + car.model + " " + car.price.ToString());
+            }
+        }
+    }
+   
+}
